a8514811d3c038d02378607e22e04e0700da42b5
adityarao310 <hello@adityarao.name>
2017-12-01 22:47:50 +0530

    Added js to bottom

--- js.js
+++ js.js
@@ -7,27 +7,28 @@ function insertHands() {
   clock.appendChild(divSeconds);
 }
 */
+const secondsHand = document.querySelector('.second-hand');
+const minutesHand = document.querySelector('.min-hand');
+const hoursHand = document.querySelector('.hour-hand');
 
 function setDate() {
   const now = new Date();
   // move seconds hand
   const nowSeconds = now.getSeconds();
   const nowSecondsDegrees = ((nowSeconds/60) * 360) + 90;
-  const secondsHand = document.querySelector('.second-hand');
   // console.log(nowSeconds);
   secondsHand.style.transform = `rotate(${nowSecondsDegrees}deg)`;
 
   // move minutes hands
   const nowMinutes = now.getMinutes();
   const nowMinutesDegrees = ((nowMinutes/60) * 360) + 90;
-  const minutesHand = document.querySelector('.min-hand');
+
   // console.log(nowMinutes);
   minutesHand.style.transform = `rotate(${nowMinutesDegrees}deg)`;
 
   // move hours hands
   const nowHours = now.getHours();
   const nowHoursDegrees = ((nowHours/12) * 360) + 90;
-  const hoursHand = document.querySelector('.hour-hand');
   // console.log(nowHours);
   hoursHand.style.transform = `rotate(${nowHoursDegrees}deg)`;
 }
